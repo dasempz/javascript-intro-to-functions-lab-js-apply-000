e16b6a161f0c5fe344cb54eba84bf0e32609bfd2
Andrea Haveman <andhaveman@hotmail.com>
2019-02-23 22:31:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var lowercase = 'hello';
-  lowercase.toLowerCase() === lowercase; // true
+  string.toLowerCase() === string; // true
   return "I can\'t hear you!";
 }
 

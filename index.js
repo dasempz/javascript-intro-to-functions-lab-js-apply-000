583cc0d56e2a3030fe872e05c72b7253331bd19c
Andrea Haveman <andhaveman@hotmail.com>
2019-02-23 21:16:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  const spy = 'HELLO';
+  console.log(spy);
+}
+function logWhisper(string) {
+  const spy = 'hello';
+  console.log(spy);
+}
+
+
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello';
+  lowercase.toLowerCase() === lowercase; // true
+  return "I can\'t hear you!";
+}
+function sayHiToGrandma(string) {
+  var uppercase = 'HELLO';
+  uppercase.toUpperCase() === uppercase; // true
+  return "YES INDEED!";
+}
+function sayHiToGrandma(string) {
+  var mixedCase = 'I love you, Grandma."',
+  string === "I love you, Grandma.";
+  return "I can\'t hear you!";
+}
